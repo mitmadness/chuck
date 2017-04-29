ee2716dff46a18cb80c3c7aa7aa40688b8b18735
Unknown <mtouverey@methodinthemadness.eu>
2017-04-29 19:18:37 +0200

    feat(express_utils): implement safeOutData() utility

--- src/express_utils.ts
+++ src/express_utils.ts
@@ -1,5 +1,15 @@
 import { Request, RequestHandler, Response, NextFunction } from 'express';
+import { Document } from 'mongoose';
 
 export function wrapAsync(handler: RequestHandler): RequestHandler {
     return (req: Request, res: Response, next: NextFunction) => handler(req, res, next).catch(next);
+}
+
+export function safeOutData<T extends Document>(document: T): Partial<T> {
+    const safeDocument = document.toObject() as any;
+
+    delete safeDocument.__v;
+    delete safeDocument._id;
+
+    return safeDocument;
 }
