0df43455ec3927cbc7845f57b79b010c28276fd0
Unknown <mtouverey@methodinthemadness.eu>
2017-04-30 17:58:20 +0200

    feat(api): implement authorization on /api + refactor

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,39 +1,13 @@
 import * as express from 'express';
-import { Request, Response, NextFunction } from 'express';
 import conversionsApi from './conversions_api';
-import logger from '../logger';
-import { HttpError } from './http_errors';
+import { errorHandler, hasValidApiKey } from './middlewares';
 
 const router = express.Router();
 
+router.use(hasValidApiKey());
+
 router.use('/conversions', conversionsApi);
 
-router.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    //=> Headers already sent, let Express handle the thing
-    if (res.headersSent) {
-        return next(err);
-    }
-
-    //=> Mongoose validation errors
-    if (err.name === 'ValidationError') {
-        return res.status(400).json(err);
-    }
-
-    if (err instanceof HttpError) {
-        return res.status(err.statusCode).json({
-            name: err.constructor.name,
-            message: err.message
-        });
-    }
-
-    //=> In all other cases, return a 500 error
-    res.status(500).json({
-        name: 'FatalError',
-        message: 'Fatal server error - please retry'
-    });
-
-    //=> Log it because that's unexpected
-    logger.error(err);
-});
+router.use(errorHandler());
 
 export default router;
---
+++ src/api/middlewares.ts
@@ -0,0 +1,53 @@
+import { ErrorRequestHandler, Handler, NextFunction, Request, Response } from 'express';
+import logger from '../logger';
+import { ApiKey } from '../models';
+import { HttpError, UnauthorizedError } from './http_errors';
+import { wrapAsync } from '../express_utils';
+
+export function hasValidApiKey(): Handler {
+    return wrapAsync(async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        const authHeader = req.header('Authorization');
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            throw new UnauthorizedError('The API key must be provided in the Authorization header, with scheme Bearer');
+        }
+
+        const apiKey = authHeader.split(' ')[1];
+        const apiKeyDocument = await ApiKey.findOne({ key: apiKey });
+
+        if (!apiKeyDocument) {
+            throw new UnauthorizedError(`The API key ${apiKey} does not seem to exist`);
+        }
+
+        next();
+    });
+}
+
+export function errorHandler(): ErrorRequestHandler {
+    return (err: any, req: Request, res: Response, next: NextFunction): void => {
+        //=> Headers already sent, let Express handle the thing
+        if (res.headersSent) {
+            return void next(err);
+        }
+
+        //=> Mongoose validation errors
+        if (err.name === 'ValidationError') {
+            return void res.status(400).json(err);
+        }
+
+        if (err instanceof HttpError) {
+            return void res.status(err.statusCode).json({
+                name: err.constructor.name,
+                message: err.message
+            });
+        }
+
+        //=> In all other cases, return a 500 error
+        res.status(500).json({
+            name: 'FatalError',
+            message: 'Fatal server error - please retry'
+        });
+
+        //=> Log it because that's unexpected
+        logger.error(err);
+    };
+}
