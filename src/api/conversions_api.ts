44448ffd12a257706be1d2ac0ba65a6896e9d76f
Unknown <mtouverey@methodinthemadness.eu>
2017-04-30 17:15:37 +0200

    feat(conversions_api): add GET /api/conversions/:code endpoint

--- src/api/conversions_api.ts
+++ src/api/conversions_api.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import { Conversion, safeData as safeConversionData } from '../models/conversion';
 import { wrapAsync, safeOutData } from '../express_utils';
+import { NotFoundError } from './http_errors';
 
 const router = express.Router();
 
@@ -9,6 +10,17 @@ router.post('/', wrapAsync(async (req, res, next) => {
     const conversion = await Conversion.create(conversionData);
 
     res.status(202).json(safeOutData(conversion));
+
+    next();
+}));
+
+router.get('/:code', wrapAsync(async (req, res, next) => {
+    const conversion = await Conversion.findOne({ code: req.params.code });
+    if (!conversion) {
+        throw new NotFoundError();
+    }
+
+    res.status(200).json(safeOutData(conversion));
 
     next();
 }));
