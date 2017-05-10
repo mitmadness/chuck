d48704983bb43967d310f2547dc18e0f1a87f2b9
Tristan Zimpfer <tristan.zimpfer@methodinthemadness.eu>
2017-05-10 17:47:31 +0200

    style(download_asset): fix indentation and library importation order.

--- src/converter/steps/01_download_assets.ts
+++ src/converter/steps/01_download_assets.ts
@@ -1,9 +1,9 @@
-import { IConversionJob } from '../job';
-import { IStepDescription } from './step';
 import * as https from 'https';
 import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
+import { IConversionJob } from '../job';
+import { IStepDescription } from './step';
 
 export function describe(): IStepDescription {
     return {
@@ -17,32 +17,37 @@ export function shouldProcess(job: IConversionJob) {
     return true;
 }
 
-export function process(job: IConversionJob): Promise<{}> {
+export function process(job: IConversionJob) {
     job.context = {tempAssets: []};
     const promises = [];
+
     for(const url of job.data.assetUrls){
         const filename = url.split('/').reverse()[0];
         const filepath = os.tmpdir() + path.sep + Date.now() + '-' + filename;
 
-        const p = new Promise((resolve,reject) =>
-        https.get(url, (response) => {
-            if(response.statusCode != 200) {
-                reject(`error ${ response.statusCode } for ${url}`);
-            }else{
+        const p = new Promise<void>((resolve, reject) => {
+            https.get(url, (response) => {
+                if(response.statusCode != 200) {
+                    reject(new Error(`error ${ response.statusCode } for ${url}`));
+                    return;
+                }
+
                 const file = fs.createWriteStream(filepath);
                 response.pipe(file);
+
                 file.on('finish', () => {
                     file.close();
                     job.context.tempAssets.push(filepath);
                     resolve();
                 });
-            }
-        }).on('error', (err) => {
-            fs.unlink(filepath);
-            reject(err);
-            }));
+            }).on('error', (err) => {
+                fs.unlink(filepath);
+                reject(err);
+            });
+        });
 
         promises.push(p);
     }
-    return Promise.all(promises);
+
+    return Promise.all(promises) as Promise<void>;
 }
