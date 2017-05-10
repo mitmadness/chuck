1fa78f09f43d05fc30cc4ed34f9b8a41804ed0bb
Unknown <mtouverey@methodinthemadness.eu>
2017-05-10 23:45:20 +0200

    refactor(01_download_assets): use a temp subdirectory + CS

--- src/converter/steps/01_download_assets.ts
+++ src/converter/steps/01_download_assets.ts
@@ -1,7 +1,9 @@
+import { IncomingMessage } from 'http';
 import * as https from 'https';
 import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
+import * as pify from 'pify';
 import { IConversionJob } from '../job';
 import { IStepDescription } from './step';
 
@@ -13,41 +15,53 @@ export function describe(): IStepDescription {
     };
 }
 
-export function shouldProcess(job: IConversionJob) {
+export function shouldProcess(job: IConversionJob): boolean {
     return true;
 }
 
-export function process(job: IConversionJob) {
-    job.context = {tempAssets: []};
-    const promises = [];
+export async function process(job: IConversionJob): Promise<void> {
+    // @TODO add progress report
 
-    for(const url of job.data.assetUrls){
-        const filename = url.split('/').reverse()[0];
-        const filepath = os.tmpdir() + path.sep + Date.now() + '-' + filename;
+    job.context = { tempAssets: [] };
 
-        const p = new Promise<void>((resolve, reject) => {
-            https.get(url, (response) => {
-                if(response.statusCode != 200) {
-                    reject(new Error(`error ${ response.statusCode } for ${url}`));
-                    return;
-                }
+    //=> Create a temporary folder for the assets
+    const tmpDir = path.resolve(`${os.tmpdir()}/chuck-dl-assets-${Date.now()}`);
+    await pify(fs.mkdir)(tmpDir);
 
-                const file = fs.createWriteStream(filepath);
-                response.pipe(file);
+    //=> Start downloading all assets
+    const promises = job.data.assetUrls.map(url => downloadAndStoreAsset(url, tmpDir));
 
-                file.on('finish', () => {
-                    file.close();
-                    job.context.tempAssets.push(filepath);
-                    resolve();
-                });
-            }).on('error', (err) => {
-                fs.unlink(filepath);
-                reject(err);
-            });
+    //=> Await downloads and store resulting paths in the context
+    job.context.tempAssets = await Promise.all(promises);
+}
+
+async function downloadAndStoreAsset(assetUrl: string, directory: string): Promise<string> {
+    //=> Find the dest path for the asset based on `directory`
+    const fileName = assetUrl.split('/').reverse()[0]; // @TODO Trim query strings and anchors
+    const filePath = path.resolve(`${directory}/${fileName}`);
+
+    //=> Make HTTP request and create a write stream to the temp asset file
+    const onlineAsset = await downloadAsset(assetUrl);
+    const localAsset = fs.createWriteStream(filePath);
+
+    //=> Pipe the HTTP response content to the write stream, then resolve
+    return new Promise<string>((resolve) => {
+        onlineAsset.pipe(localAsset).on('finish', () => {
+            localAsset.close();
+            resolve(filePath);
         });
+    });
+}
 
-        promises.push(p);
-    }
+function downloadAsset(assetUrl: string): Promise<IncomingMessage> {
+    return new Promise((resolve, reject) => {
+        https.get(assetUrl, (response) => { // @TODO Support http links!
+            if (response.statusCode != 200) {
+                reject(new Error(`HTTP error ${response.statusCode} for ${assetUrl}`));
+                return;
+            }
 
-    return Promise.all(promises) as Promise<void>;
+            resolve(response);
+        }).on('error', reject);
+    });
 }
