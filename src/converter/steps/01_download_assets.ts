da6793627279b257d980a56108ec94f116226eac
Tristan Zimpfer <tristan.zimpfer@methodinthemadness.eu>
2017-05-12 14:19:13 +0200

    feat(download_assets): add progress report and support for http links, trim query strings and anchors from filename and fix the Promise.all(downloads)

--- src/converter/steps/01_download_assets.ts
+++ src/converter/steps/01_download_assets.ts
@@ -1,9 +1,10 @@
-import { IncomingMessage } from 'http';
+import * as http from 'http';
 import * as https from 'https';
 import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 import * as pify from 'pify';
+import * as url from 'url';
 import { IConversionJob } from '../job';
 import { IStepDescription, IStepsContext } from './step';
 
@@ -25,17 +26,39 @@ export function shouldProcess(job: IConversionJob): boolean {
 }
 
 export async function process(job: IConversionJob, context: IDownloadAssetsStepsContext): Promise<void> {
-    // @TODO add progress report
     //=> Create a temporary folder for the assets
     const tmpDir = path.resolve(`${os.tmpdir()}/chuck-dl-assets-${Date.now()}`);
     await pify(fs.mkdir)(tmpDir);
 
-    //=> Start downloading all assets
-    const downloads = job.data.assetUrls.map(url => downloadAndStoreAsset(url, tmpDir));
-
-    //=> Await downloads and store resulting paths in the context
     context.downloadedAssetsDir = tmpDir;
-    context.downloadedAssetsPaths = await Promise.all(downloads);
+    context.downloadedAssetsPaths = [];
+
+    //=> Start downloading all assets
+    const downloads = job.data.assetUrls.map(async (url) => {
+        await job.progress({ type: 'download-assets', message : `Downloading "${url}"` });
+        await downloadAndStoreAsset(context, url, tmpDir);
+    });
+
+    //=> Await downloads and check if there are errors.
+    let remainingDownloads = downloads.length;
+    const errors: any[] = [];
+
+    return new Promise<void>((resolve, reject) => {
+        downloads.forEach((dl) => {
+            dl.then(() => {
+                if (--remainingDownloads == 0) {
+                    if (errors.length) {
+                        reject(new ExtendedError('Error(s) while downloadings', errors));
+                    } else {
+                        resolve();
+                    }
+                }
+            }).catch(err => {
+                --remainingDownloads;
+                errors.push(err);
+            });
+        });
+    });
 }
 
 export async function cleanup(context: Readonly<IDownloadAssetsStepsContext>): Promise<void> {
@@ -45,9 +68,13 @@ export async function cleanup(context: Readonly<IDownloadAssetsStepsContext>): P
     await pify(fs.rmdir)(context.downloadedAssetsDir);
 }
 
-async function downloadAndStoreAsset(assetUrl: string, directory: string): Promise<string> {
+async function downloadAndStoreAsset(
+    context: IDownloadAssetsStepsContext,
+    assetUrl: string,
+    directory: string
+): Promise<string> {
     //=> Find the dest path for the asset based on `directory`
-    const fileName = assetUrl.split('/').reverse()[0]; // @TODO Trim query strings and anchors
+    const fileName = assetUrl.split(/[?#]/)[0].split('/').reverse()[0];
     const filePath = path.resolve(`${directory}/${fileName}`);
 
     //=> Make HTTP request and create a write stream to the temp asset file
@@ -58,20 +85,34 @@ async function downloadAndStoreAsset(assetUrl: string, directory: string): Promi
     return new Promise<string>((resolve) => {
         onlineAsset.pipe(localAsset).on('finish', () => {
             localAsset.close();
+            context.downloadedAssetsPaths.push(filePath);
+
             resolve(filePath);
         });
     });
 }
 
-function downloadAsset(assetUrl: string): Promise<IncomingMessage> {
+function downloadAsset(assetUrl: string): Promise<http.IncomingMessage> {
     return new Promise((resolve, reject) => {
-        https.get(assetUrl, (response) => { // @TODO Support http links!
+        chooseGetProtocol(assetUrl)(assetUrl, (response) => {
             if (response.statusCode != 200) {
                 reject(new Error(`HTTP error ${response.statusCode} for ${assetUrl}`));
                 return;
             }
 
             resolve(response);
-        }).on('error', reject);
+        });
     });
+}
+
+class ExtendedError extends Error {
+    constructor(message: string, public errors: any[]) {
+        super(message);
+
+        Object.setPrototypeOf(this, ExtendedError.prototype);
+    }
+}
+
+function chooseGetProtocol(inputUrl: string) {
+    return url.parse(inputUrl).protocol == "https:" ? https.get : http.get;
 }
