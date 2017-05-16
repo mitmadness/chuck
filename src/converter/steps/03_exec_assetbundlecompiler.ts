3ff9cc33e77a51da203d278a86921c48528c1496
Tristan Zimpfer <tristan.zimpfer@methodinthemadness.eu>
2017-05-16 11:11:57 +0200

    feat(exec_assetbundlecompiler): implement the creation of the AssetBundle.

--- src/converter/steps/03_exec_assetbundlecompiler.ts
+++ src/converter/steps/03_exec_assetbundlecompiler.ts
@@ -1,5 +1,16 @@
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
+import * as pify from 'pify';
+import { BuildTargets, bundle} from '@mitm/assetbundlecompiler';
 import { IConversionJob } from '../job';
-import { IStepDescription } from './step';
+import { IStepDescription, IStepsContext } from './step';
+
+const { WebGL } = BuildTargets;
+
+export interface IAssetBundleStepsContext extends IStepsContext {
+    assetBundleDir: string;
+}
 
 export function describe(): IStepDescription {
     return {
@@ -13,6 +24,13 @@ export function shouldProcess(job: IConversionJob) {
     return true;
 }
 
-export function process(job: IConversionJob): Promise<void> {
-    return Promise.resolve();
+export async function process(job: IConversionJob, context: IAssetBundleStepsContext ): Promise<void> {
+    const tmpDir = path.resolve(`${os.tmpdir()}/chuck-exec-assetbundlecompiler-${Date.now()}`);
+    await pify(fs.mkdir)(tmpDir);
+    context.assetBundleDir = tmpDir;
+
+    await bundle(...context.assetsPaths)
+        .targeting(WebGL)
+        .withLogger(async (log) => await job.progress({ type: 'exec-assetbundlecompiler', message: log }) )
+        .to(path.resolve(tmpDir, job.data.assetBundleName));
 }
