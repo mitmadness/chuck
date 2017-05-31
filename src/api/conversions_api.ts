019d5c0ac81a74b3bd5fe2c7435bc11a761e5aac
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2017-05-31 18:21:12 +0200

    feat(conversions_api): changed SSE protocol, don't hang and send errors via classic HTTP
    BREAKING CHANGE: This will likely break any SSE client that uses /api/conversions/:code/events

--- src/api/conversions_api.ts
+++ src/api/conversions_api.ts
@@ -1,13 +1,17 @@
 import * as express from 'express';
 import { sse, ISseResponse } from '@toverux/expresse';
-import { safeErrorSerialize } from '../safe_error_serialize';
-import { Conversion, safeData as safeConversionData } from '../models/conversion';
+import { Conversion, safeData as safeConversionData, IConversionModel } from '../models/conversion';
 import converterQueue from '../converter/queue';
 import { IProgressReportJob } from '../converter/job';
 import { IEvent, isQueueConversionEndedEvent } from '../converter/job_events';
 import { wrapAsync, safeOutData } from '../express_utils';
 import { GoneError, NotFoundError } from './http_errors';
 import { hasValidApiKey } from './middlewares';
+
+interface InitSseRequest extends express.Request {
+    isReplay: boolean;
+    conversion: IConversionModel;
+}
 
 const router: express.Router = express.Router();
 
@@ -36,38 +40,47 @@ router.get('/:code', wrapAsync(async (req, res, next) => {
     next();
 }));
 
-router.get('/:code/events', sse(), wrapAsync(async (req, res: ISseResponse) => {
-    const isReplay = req.query.replay !== 'false';
+router.get('/:code/events', wrapAsync(async (req: InitSseRequest, res, next) => {
+    req.isReplay = req.query.replay !== 'false';
+    req.conversion = await Conversion.findOne({ code: req.params.code });
+
+    //=> Check that we can continue
+    if (!req.conversion) {
+        throw new NotFoundError();
+    } else if (!req.isReplay && req.conversion.conversion.isCompleted) {
+        throw new GoneError('This conversion is terminated');
+    }
+
+    next();
+}), sse(), async (req: InitSseRequest, res: ISseResponse, next) => {
+    // @todo Unsafe typecast -- a Bull queue is an EventEmitter, but typings are not complete
+    const emitterQueue = (converterQueue as any) as NodeJS.EventEmitter;
     const sse = sseSend.bind(null, req.query.sseType);
 
-    //=> Find the conversion
-    const conversion = await Conversion.findOne({ code: req.params.code });
-    if (!conversion) {
-        return sse(res, { type: 'error', ...safeErrorSerialize(new NotFoundError()) });
-    } else if (!isReplay && conversion.conversion.isCompleted) {
-        return sse(res, { type: 'error', ...safeErrorSerialize(new GoneError('This conversion is terminated')) });
-    }
-
     //=> Replay mode: dump all progress records
-    if (isReplay && conversion.conversion.logs) {
-        conversion.conversion.logs.forEach(progress => sse(res, progress));
-    }
+    if (req.isReplay && req.conversion.conversion.logs) {
+        req.conversion.conversion.logs.forEach(event => sse(res, event));
 
-    //=> @todo Unsafe typecast -- a Bull queue is an EventEmitter, but typings are not complete
-    const emitterQueue = (converterQueue as any) as NodeJS.EventEmitter;
+        // optim: conversion is terminated, don't watch for subsequent events
+        if (req.conversion.conversion.isCompleted) return next();
+    }
 
     //=> Listen queue for progress events, filter, and send if the jobId matches
     emitterQueue.on('progress', handleProgress);
 
     function handleProgress(job: IProgressReportJob, event: IEvent): void {
-        if (job.id != conversion.conversion.jobId) return;
+        if (job.id != req.conversion.conversion.jobId) return;
         sse(res, event);
 
         if (isQueueConversionEndedEvent(event)) {
             emitterQueue.removeListener('progress', handleProgress);
+            next();
         }
     }
-}));
+}, (req, res, next) => {
+    res.end(); // avoid hanging for nothing, would be too easy to DDoS it
+    next();
+});
 
 function sseSend(type: 'events'|'data', res: ISseResponse, event: IEvent): void {
     if (type == 'data') {
