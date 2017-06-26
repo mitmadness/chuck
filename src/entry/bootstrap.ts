07341ce4dc98183556ac9cf6bb7ba230beac20f4
Unknown <mtouverey@methodinthemadness.eu>
2017-06-26 23:17:19 +0200

    feat(bootstrap): configure raven's uncaught exceptions/rejections handler

--- src/entry/bootstrap.ts
+++ src/entry/bootstrap.ts
@@ -1,15 +1,19 @@
+import * as raven from 'raven';
 import * as sourceMapSupport from 'source-map-support';
-import '../config';
+import config from '../config';
 import logger from '../logger';
 
 sourceMapSupport.install();
 
+config.ravenDsn && raven.config(config.ravenDsn, {
+    release: config.release,
+    environment: config.env
+}).install();
+
 process.on('unhandledRejection', (reason: any): void => {
     logger.error('UNHANDLED REJECTION', reason);
-    process.exit(1);
 });
 
 process.on('uncaughtException', (err: any): void => {
     logger.error('UNCAUGHT EXCEPTION', err);
-    process.exit(1);
 });
